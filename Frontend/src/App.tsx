07d7bb89e5f9c24bfc828dc776af5f7497ebc192
Aniket Kole <aniketkole07@gmail.com>
2026-07-16 12:45:00 +0530

    Intial project setup

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <div className="App max-w-7xl mt-10 mx-auto">
+        <h1 className="text-center text-2xl">Clinic Management System</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
